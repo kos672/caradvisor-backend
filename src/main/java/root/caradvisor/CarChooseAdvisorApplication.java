48901b9d8b311a5f712b9d6901082db7790fe054
Konstantin <kocc95@mail.ru>
2018-09-29 12:32:21 +0200

    initial commit

---
+++ src/main/java/root/caradvisor/CarChooseAdvisorApplication.java
@@ -0,0 +1,12 @@
+package root.caradvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarChooseAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarChooseAdvisorApplication.class, args);
+    }
+}

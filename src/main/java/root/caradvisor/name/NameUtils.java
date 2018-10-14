1510fe6ac2947ff8f3d5f6509f59d3ecca24f468
Konstantin <kocc95@mail.ru>
2018-10-14 14:01:34 +0200

    NameUtils class is implemented with test

---
+++ src/main/java/root/caradvisor/name/NameUtils.java
@@ -0,0 +1,56 @@
+package root.caradvisor.name;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import root.caradvisor.model.Gender;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@Component
+@Slf4j
+public class NameUtils {
+
+    public String replacePolishCharacters(String name) {
+        return name.toLowerCase()
+                .replaceAll("ł", "l")
+                .replaceAll("ę", "e")
+                .replaceAll("ó", "o")
+                .replaceAll("ą", "a")
+                .replaceAll("ś", "s")
+                .replaceAll("ż", "z")
+                .replaceAll("ź", "z")
+                .replaceAll("ć", "c");
+    }
+
+    public String recognizeGender(String name) {
+        String nameWithoutPolishCharacters = replacePolishCharacters(name);
+        String menNamesFilePath = "src/main/resources/men_names.txt";
+        String femaleNamesFilePath = "src/main/resources/female_names.txt";
+        List<String> menNames = new ArrayList<>();
+        List<String> femaleNames = new ArrayList<>();
+        String recognizedGenderCharacter = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(menNamesFilePath)))) {
+            menNames.addAll(asList(reader.readLine().split(",")));
+        } catch (IOException e) {
+            log.error("File with specified path not found: %s", menNamesFilePath);
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(femaleNamesFilePath)))) {
+            femaleNames.addAll(asList(reader.readLine().split(",")));
+        } catch (IOException e) {
+            log.error("File with specified path not found: %s", menNamesFilePath);
+        }
+        if (menNames.contains(nameWithoutPolishCharacters)) {
+            recognizedGenderCharacter = Gender.MALE.getGenderSign();
+        } else if (femaleNames.contains(nameWithoutPolishCharacters)) {
+            recognizedGenderCharacter = Gender.FEMALE.getGenderSign();
+        }
+        return recognizedGenderCharacter;
+    }
+}
---
+++ src/test/java/root/caradvisor/name/NameUtilsTest.java
@@ -0,0 +1,73 @@
+package root.caradvisor.name;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NameUtilsTest {
+
+    @Autowired
+    private NameUtils nameUtils;
+
+    @Test
+    public void polishCharactersShouldBeSuccessfullyReplaced() {
+        // given
+        String name1 = "Łukasz";
+        String name2 = "Włodzimierz";
+        String name3 = "Bożena";
+
+        // when
+        String replaced1 = nameUtils.replacePolishCharacters(name1);
+        String replaced2 = nameUtils.replacePolishCharacters(name2);
+        String replaced3 = nameUtils.replacePolishCharacters(name3);
+
+        // then
+        assertFalse(replaced1.contains("ł"));
+        assertFalse(replaced2.contains("ł"));
+        assertFalse(replaced3.contains("ż"));
+    }
+
+    @Test
+    public void shouldRecgonizeNameAsMale() {
+        // given
+        String name = "Grzegorz";
+
+        // when
+        String recognizeGender = nameUtils.recognizeGender(name);
+
+        // then
+        assertThat(recognizeGender, equalTo("m"));
+    }
+
+    @Test
+    public void shouldRecgonizeNameAsFemale() {
+        // given
+        String name = "Katarzyna";
+
+        // when
+        String recognizeGender = nameUtils.recognizeGender(name);
+
+        // then
+        assertThat(recognizeGender, equalTo("f"));
+    }
+
+    @Test
+    public void shouldDontRecognizeName() {
+        // given
+        String name = "Kata";
+
+        // when
+        String recognizeGender = nameUtils.recognizeGender(name);
+
+        // then
+        assertThat(recognizeGender, equalTo(""));
+    }
+}

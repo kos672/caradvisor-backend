9a84c8021358027a55128dff3c14326332be4f52
Konstantin <kocc95@mail.ru>
2018-10-21 18:31:10 +0200

    changed method type and implementation

--- src/main/java/root/caradvisor/endpoint/NameController.java
+++ src/main/java/root/caradvisor/endpoint/NameController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import root.caradvisor.name.NameUtils;
 
@@ -20,11 +20,12 @@ public class NameController {
         this.nameUtils = nameUtils;
     }
 
-    @GetMapping(value = "/name", produces = "application/json")
-    public ResponseEntity<String> sendName(@RequestParam(value = "name") String name) {
+    @PostMapping(value = "/name", produces = "application/json")
+    public ResponseEntity<String> sendName(@RequestBody String name) {
         String gender = nameUtils.recognizeGender(name);
+        String response = String.format("{\"gender\": \"%s\"}", gender);
         return !gender.equals("") ?
-                new ResponseEntity<>(gender, HttpStatus.OK) :
-                new ResponseEntity<>(gender, HttpStatus.BAD_REQUEST);
+                new ResponseEntity<>(response, HttpStatus.OK) :
+                new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
